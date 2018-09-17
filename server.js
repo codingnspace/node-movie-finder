b4c896c31fee4708f3fdbf27ced08dd13c3aa810
pearl1991 <pmcphee@healthleadsusa.org>
2018-09-16 22:17:18 -0400

    fixing heroku deployment error with static port number

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.get('/movie/:id', function(req, res){
   });
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
 });
