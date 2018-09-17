ec894ca1cfee56ae3eb595c320b54abae509520c
pearl1991 <pmcphee@healthleadsusa.org>
2018-09-16 22:02:36 -0400

    added favorite feature and movie detail page

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-// app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-// app.use(express.static(path.join(__dirname, "public")));
 app.get('/', function (req, res) {
   res.render('index');
 });
@@ -32,23 +33,46 @@ app.post('/', function (req, res) {
 })
 
 app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  res.render('favorites', {favorites: data})
 });
 
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
+app.post('/favorites', function(req, res){
+  var movieId = req.body.movie_id;
+  if(!movieId){
     res.send("Error");
     return
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
   }
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  var url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`;
+  request(url, function (err, response, body) {
+    if(err){
+      res.send('Error, please try again');
+    } else {
+      var movie = JSON.parse(body);
+      data.push(movie);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data); 
+    }
+  });
 });
+
+app.get('/movie/:id', function(req, res){
+  var movieId = req.params.id;
+  if(!movieId){
+    res.send("Error");
+    return
+  }
+  var url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`;
+  request(url, function (err, response, body) {
+    if(err){
+      res.send('Error, please try again');
+    } else {
+      var movie = JSON.parse(body);
+      res.render('movie', {movie}); 
+    }
+  });
+})
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
